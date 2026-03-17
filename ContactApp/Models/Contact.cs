01e043f412860a4529efd4a59a8f57b06a36078f
unknown <shazaly.se@gmail.com>
2026-03-17 23:03:51 +0400

    update DB and files by github cicd actions

--- ContactApp/Models/Contact.cs
+++ ContactApp/Models/Contact.cs
@@ -13,6 +13,11 @@ namespace ContactApp.Models
         [Required]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Phone { get; set; }
         // Store the file name or path of the uploaded image
         public string? ProfileImage { get; set; }
     }
