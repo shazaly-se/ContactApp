62a6772329f31d416cf7951a356c91ae3e15892c
unknown <shazaly.se@gmail.com>
2026-03-20 00:08:05 +0400

    application insights in Home logged

--- ContactApp/Controllers/HomeController.cs
+++ ContactApp/Controllers/HomeController.cs
@@ -8,13 +8,16 @@ namespace ContactApp.Controllers
     public class HomeController : Controller
     {
         private readonly TelemetryClient _telemetry;
+        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(TelemetryClient telemetry)
+        public HomeController(TelemetryClient telemetry, ILogger<HomeController> logger)
         {
             _telemetry = telemetry;
+            _logger = logger;
         }
         public IActionResult Index()
         {
+            _logger.LogInformation("Index page logged");
             _telemetry.TrackTrace("User visited Home page");
             _telemetry.TrackEvent("HomePageLoaded");
             return View();
